5deafa422d4b8f4e38d212dd72d03de9e3986574
Ayşe Nur Gökkaya <34404993+aysenurgokkaya@users.noreply.github.com>
2022-04-03 15:37:37 +0300

    Delete Topics directory

--- Topics/Printing data/From 0 to 4/src/Main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-    public static void main(String[] args) {  
-        for (int i = 0; i <= 4; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- Topics/Printing data/The result of a Tic-Tac-Toe game/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-
-class Main {
-    public static void main(String[] args) {
-        System.out.println("O X X\n" +
-                "O X O\n" +
-                "X O X");
-    }
-}
